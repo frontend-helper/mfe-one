efba24dba7cb80e7fe832ae444fee6d79f0521a8
frontend-helper <108347339+frontend-helper@users.noreply.github.com>
2023-06-06 11:47:34 +0530

    CDN path changed.

--- concat.js
+++ concat.js
@@ -5,5 +5,5 @@ const concat = require("concat");
     "dist/mfe1/polyfills.js",
     "dist/mfe1/runtime.js",
   ];
-  await concat(files, "dist/mfe1/mfe-one.js");
+  await concat(files, "cdn/mfe-one.js");
 })();
